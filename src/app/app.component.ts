8314384acb43b2158f5a840226d754d40393b659
Bruno <bruno@Brunos-MacBook-Pro.local>
2018-12-13 18:05:37 +0100

    added cmg text in Textview

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 // import { DataService } from "./services/data/data.service";
 import { JsonToD3Service } from "./services/jsonToD3/json-to-d3.service";
 
